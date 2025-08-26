7eee0814960eb0bc02ca2e89f36ac67a8a76f6a2
Fernanda Cavalcante <cavalcantefernanda552@gmail.com>
2025-08-25 21:25:56 -0300

    Add files via upload

---
+++ yfnand.py
@@ -0,0 +1,11 @@
+bolos=float(input("""escolha um sabor de bolo:
+1=chocolate
+2=morango
+3=redvelvet
+"""))
+if bolos==1:
+    print("chocolate")
+elif bolos==2:
+    print("morango")
+else:
+    print("redvelvet")
